d7d8f126b40d8f1a306948d0e1ee2effc5097406
ilmerry <milkiss0414@naver.com>
2022-01-10 23:05:23 +0900

    ch04-2-event-rsc

--- src/EventPractive.js
+++ src/EventPractive.js
@@ -1,38 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class EventPractive extends Component {
-  state = {
-    title: "이벤트 연습",
+const EventPractive = () => {
+  const [form, setForm] = useState({
+    username: "",
     message: "",
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존 form 복사
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
   };
-  render() {
-    return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요"
-          value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
-        />
-        <button
-          onClick={() => {
-            this.setState({
-              title: this.state.message,
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
-      </div>
-    );
-  }
-}
+  const onClick = () => {
+    setForm({
+      username: "",
+      message: "",
+    });
+  };
+
+  return (
+    <div>
+      <h1>이름: {username}</h1>
+      <h1>메시지: {message}</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="이름을 입력하세요"
+        value={username}
+        onChange={onChange}
+      />
+      <br />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={message}
+        onChange={onChange}
+      />
+      <br />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractive;
