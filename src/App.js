61a0cdc937c3ef1605909e1976487e3083d0eda6
ilmerry <milkiss0414@naver.com>
2022-01-13 02:05:00 +0900

    ch05-1-ref

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractive from "./EventPractive";
+import ValidationSample from "./ValidationSample";
 
 const App = () => {
-  return <EventPractive />;
+  return <ValidationSample />;
 };
 
 export default App;
---
+++ src/ValidationSample.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./ValidationSample.css";
+
+// 비밀번호가 맞았으면 초록색, 틀렸으면 빨간색으로 input 색상을 바꾸는 컴포넌트
+class ValidationSample extends Component {
+  state = {
+    password: "",
+    clicked: false,
+    validated: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === "0000", // boolean
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? "success"
+                : "failure"
+              : ""
+          }
+        />
+        <button onClick={this.handleButtonClick}>검증하기</button>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
