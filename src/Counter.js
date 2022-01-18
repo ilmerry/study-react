97c212c8f5b4b3d91afbf4215f6a5009d11e9ee2
ilmerry <milkiss0414@naver.com>
2022-01-19 00:32:05 +0900

    ch08-hooks-reducer-counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
-class Counter extends Component {
-  state = {
-    number: 0,
+function reducer(state, action) {
+  console.log(parseInt(action.value));
+  return {
+    value: state.value + parseInt(action.value),
   };
-  render() {
-    // number 조회시 this.state.number 방지
-    const { number } = this.state;
-    return (
-      <div>
-        <h1>{number}</h1>
-        <button
-          onClick={() => {
-            this.setState(
-              {
-                number: number + 1,
-              },
-              () => {
-                // 콜백함수: 값 업데이트하고 난 후 실행됨
-                console.log("방금 setState가 호출되었습니다.");
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          +1
-        </button>
-      </div>
-    );
-  }
 }
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  const handleClick = (e) => {
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <h1>현재 카운터 값: {state.value}</h1>
+      <button value={+1} onClick={handleClick}>
+        +1
+      </button>
+      <br />
+      <button value={-1} onClick={handleClick}>
+        -1
+      </button>
+    </div>
+  );
+};
 
 export default Counter;
