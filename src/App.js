c1cecd07e15df4e7907ea406cd8f05ab9d5eb665
ilmerry <milkiss0414@naver.com>
2022-01-19 14:02:16 +0900

    ch09-scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Average from "./Average";
 import Counter from "./Counter";
+import SassComponent from "./SassComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Counter />
+        <SassComponent />
       </div>
     );
   }
