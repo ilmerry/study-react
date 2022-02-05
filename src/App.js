8ec3ba3f3564e204356085abbeb50d73e93736bc
ilmerry <milkiss0414@naver.com>
2022-02-05 21:50:35 +0900

    ch15-context-color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-// newsapi password :: studyingreact9284
-// API :: 4c858a1b67a747289b7d4d56bc82784a
-import React, { useState, useCallback } from 'react';
-import { Route, Routes, useParams } from '../node_modules/react-router/index';
-import Categories from './news-app/Categories';
-import NewsList from './news-app/NewsList';
-import NewsPage from './news-app/NewsPage';
+import React from 'react';
+import ColorBox from './components/ColorBox';
+import SelectColors from './components/SelectColors';
+import { ColorProvider } from './contexts/color';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<NewsPage />} exact={true} />
-      <Route path="/business" element={<NewsPage match={'business'} />} />
-      <Route
-        path="/entertainment"
-        element={<NewsPage match={'entertainment'} />}
-      />
-      <Route path="/health" element={<NewsPage match={'health'} />} />
-      <Route path="/science" element={<NewsPage match={'science'} />} />
-      <Route path="/sports" element={<NewsPage match={'sports'} />} />
-      <Route path="/technology" element={<NewsPage match={'technology'} />} />
-    </Routes>
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
   );
 };
 
