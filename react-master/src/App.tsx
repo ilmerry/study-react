44c3434212b9f013f7fda1857cac4151fd133a45
Eunhyung Choi <milkiss0414@naver.com>
2022-08-09 16:15:14 +0900

    RMC: KanBan - dnd 구현

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -72,22 +72,27 @@ function App() {
   const onDragEnd = () => {};
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {() => (
-            <ul>
-              <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
-              </Draggable>
-            </ul>
-          )}
-        </Droppable>
-      </div>
-    </DragDropContext>
+    <>
+      <GlobalStyle/>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div>
+          <Droppable droppableId="one">
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+                <Draggable draggableId="first" index={0}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    One
+                  </li>
+                )}
+                </Draggable>
+              </ul>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
+    </>
   );
 }
 
