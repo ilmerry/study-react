65191e659a384d5053a72f6f0bf0471dcb7073a2
Eunhyung Choi <milkiss0414@naver.com>
2022-08-15 19:11:09 +0900

    install framer-motion

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -1,119 +1,137 @@
 import Router from './Router';
-import { createGlobalStyle } from 'styled-components';
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
+// import { createGlobalStyle } from 'styled-components';
+// import {
+//   DragDropContext,
+//   Droppable,
+//   Draggable,
+//   DropResult,
+// } from "react-beautiful-dnd";
+// import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { toDoState } from './atoms';
-import Board from './components/Board';
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+// import { toDoState } from './atoms';
+// import Board from './components/Board';
+// import React, { useEffect, useState } from 'react';
+// import { useForm } from 'react-hook-form';
 
+import { motion } from "framer-motion";
+
+// const Wrapper = styled.div`
+//   display: flex;
+//   max-width: 680px;
+//   width: 100vw;
+//   margin: 0 auto;
+//   justify-content: center;
+//   align-items: center;
+//   height: 100vh;
+// `;
 const Wrapper = styled.div`
   display: flex;
-  max-width: 680px;
+  height: 100vh;
   width: 100vw;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
   height: 100vh;
 `;
 
-const Boards = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
+// const Boards = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: flex-start;
+//   width: 100%;
+//   gap: 10px;
+//   grid-template-columns: repeat(3, 1fr);
+// `;
+
+// const Form = styled.form`
+//   background-color: ${(props) => props.theme.accentColor};
+//   input {
+//     width: 100%;
+//   };
+//   display: flex;
+//   justify-content: center;
+//   align-items: flex-end;
+//   flex-direction: column;
+//   padding: 10px;
+//   border-radius: 5px;
+// `;
+
+// const Button = styled.button`
+//   background-color: transparent;
+//   cursor: pointer;
+//   border: none;
+//   font-size: 15px;
+//   margin-top: 5px;
+// `
+
+// interface IForm {
+//   category: string;
+// }
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-const Form = styled.form`
-  background-color: ${(props) => props.theme.accentColor};
-  input {
-    width: 100%;
-  };
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  flex-direction: column;
-  padding: 10px;
-  border-radius: 5px;
-`;
-
-const Button = styled.button`
-  background-color: transparent;
-  cursor: pointer;
-  border: none;
-  font-size: 15px;
-  margin-top: 5px;
-`
-
-interface IForm {
-  category: string;
-}
 
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const { register, setValue, handleSubmit } = useForm<IForm>();
+  // const [toDos, setToDos] = useRecoilState(toDoState);
+  // const { register, setValue, handleSubmit } = useForm<IForm>();
 
-  const onDragEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
-    if (!destination) return;
-    if (destination?.droppableId === source.droppableId) {
-      // same board movement.
-      setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    }
-    if (destination.droppableId !== source.droppableId) {
-      // cross board movement
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  }
+  // const onDragEnd = (info: DropResult) => {
+  //   const { destination, draggableId, source } = info;
+  //   if (!destination) return;
+  //   if (destination?.droppableId === source.droppableId) {
+  //     // same board movement.
+  //     setToDos((allBoards) => {
+  //       const boardCopy = [...allBoards[source.droppableId]];
+  //       const taskObj = boardCopy[source.index];
+  //       boardCopy.splice(source.index, 1);
+  //       boardCopy.splice(destination?.index, 0, taskObj);
+  //       return {
+  //         ...allBoards,
+  //         [source.droppableId]: boardCopy,
+  //       };
+  //     });
+  //   }
+  //   if (destination.droppableId !== source.droppableId) {
+  //     // cross board movement
+  //     setToDos((allBoards) => {
+  //       const sourceBoard = [...allBoards[source.droppableId]];
+  //       const destinationBoard = [...allBoards[destination.droppableId]];
+  //       const taskObj = sourceBoard[source.index];
+  //       sourceBoard.splice(source.index, 1);
+  //       destinationBoard.splice(destination?.index, 0, taskObj);
+  //       return {
+  //         ...allBoards,
+  //         [source.droppableId]: sourceBoard,
+  //         [destination.droppableId]: destinationBoard,
+  //       };
+  //     });
+  //   }
+  // }
 
-  const onValid = ({ category }: IForm) => {
-    setToDos((allBoards) => {
-      return {
-        ...allBoards,
-        [category]: [],
-      };
-    });
-    setValue("category", "");
-  }
+  // const onValid = ({ category }: IForm) => {
+  //   setToDos((allBoards) => {
+  //     return {
+  //       ...allBoards,
+  //       [category]: [],
+  //     };
+  //   });
+  //   setValue("category", "");
+  // }
 
-  useEffect(() => {
-    Object.keys(toDos).map((key) => {
-      localStorage.setItem(key, JSON.stringify(toDos[key]));
-      console.log(JSON.parse(localStorage.getItem(key) as string))
-    })
-  }, [toDos])
+  // useEffect(() => {
+  //   Object.keys(toDos).map((key) => {
+  //     localStorage.setItem(key, JSON.stringify(toDos[key]));
+  //     console.log(JSON.parse(localStorage.getItem(key) as string))
+  //   })
+  // }, [toDos])
 
   return (
   <>
-    <GlobalStyle/>
+    {/* <GlobalStyle/>
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
@@ -130,73 +148,14 @@ function App() {
           </Form>
         </Boards>
       </Wrapper>
-    </DragDropContext>
+    </DragDropContext> */}
+    <Wrapper>
+      <Box/>
+      <motion.div></motion.div>
+    </Wrapper>
   </>
   );
 }
 
 export default App;
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  // 폰트가 작동하지 않는다면 sans-serif 사용 예정
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor};
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
-`;
